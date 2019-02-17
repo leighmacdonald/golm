4a7a04c8623467e6748a4d395fa736e8d3a3c9e7
Leigh MacDonald <leigh.macdonald@gmail.com>
2019-02-17 14:58:06 -0700

    General Golang style cleanups. Add go module.

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# libolm-go
+# golm
+
 Go Bindings for libolm, an implementation of the Double Ratchet cryptographic ratchet
+
+## Dev setup
+
+    git clone git@github.com:leighmacdonald/golm.git
+    cd golm
+    git clone https://git.matrix.org/git/olm.git olm
+    cd olm
+    cmake . -Bbuild
+    cmake --build build
+    cd ..
+    go build
+    
--- account.go
+++ account.go
@@ -1,160 +1,179 @@
-package libolm-go
+package golm
 
 /*
-#cgo LDFLAGS: -lolm
+#cgo LDFLAGS: -lolm -L./olm/build
+#cgo CFLAGS: -I./olm/include
 #include <olm/olm.h>
 #include <stdlib.h>
 */
 import "C"
 
 import (
-    //"encoding/json"
-    "fmt"
-    "crypto/rand"
-    "unsafe"
+	"crypto/rand"
+	"encoding/json"
+	"fmt"
+	"log"
+	"unsafe"
 )
 
-func GetVersion() (major byte, minor byte, patch byte){
-    var ma C.uint8_t
-    var mi C.uint8_t
-    var pa C.uint8_t
-    C.olm_get_library_version(&ma, &mi, &pa)
-    return byte(ma), byte(mi), byte(pa)
+func GetVersion() (major byte, minor byte, patch byte) {
+	var ma C.uint8_t
+	var mi C.uint8_t
+	var pa C.uint8_t
+	C.olm_get_library_version(&ma, &mi, &pa)
+	return byte(ma), byte(mi), byte(pa)
+}
+
+type IdentityKeys struct {
+	Curve25519 string `json:"curve25519"`
+	Ed25519    string `json:"ed25519"`
+}
+
+type OneTimeKeys struct {
+	Curve25519 map[string]string `json:"curve25519"`
+	Ed25519    map[string]string `json:"ed25519"`
 }
 
 type Account struct {
-    buf []byte
-    ptr *C.struct_OlmAccount
+	buf []byte
+	ptr *C.struct_OlmAccount
 }
 
-func AccountFromPickle(key string, pickle string) (Account){
-    account := newAccount()
+func AccountFromPickle(key string, pickle string) Account {
+	account := newAccount()
 
-    key_buf := []byte(key)
-    pickle_buffer := []byte(pickle)
+	keyBufs := []byte(key)
+	pickleBuffers := []byte(pickle)
 
-    // this returns a result we should probably inspect
-    C.olm_unpickle_account(
-        account.ptr,
-        unsafe.Pointer(&key_buf[0]), C.size_t(len(key_buf)),
-        unsafe.Pointer(&pickle_buffer[0]), C.size_t(len(pickle_buffer)),
-    )
+	// this returns a result we should probably inspect
+	C.olm_unpickle_account(
+		account.ptr,
+		unsafe.Pointer(&keyBufs[0]), C.size_t(len(keyBufs)),
+		unsafe.Pointer(&pickleBuffers[0]), C.size_t(len(pickleBuffers)),
+	)
 
-    fmt.Println(account.lastError())
+	fmt.Println(account.lastError())
 
-    return account
+	return account
 }
 
-func newAccount() (Account){
-    account_buf := make([]byte, C.olm_account_size())
-    olm_account := C.olm_account(unsafe.Pointer(&account_buf[0]))
+func newAccount() Account {
+	accountBufs := make([]byte, C.olm_account_size())
+	olmAccount := C.olm_account(unsafe.Pointer(&accountBufs[0]))
 
-    return Account{buf: account_buf, ptr: olm_account}
+	return Account{buf: accountBufs, ptr: olmAccount}
 }
 
-func CreateNewAccount() (Account){
-    account := newAccount()
-    rand_len := C.olm_create_account_random_length(account.ptr)
-    rand_buf := make([]byte, rand_len)
+func CreateNewAccount() Account {
+	account := newAccount()
+	randLen := C.olm_create_account_random_length(account.ptr)
+	randBufs := make([]byte, randLen)
 
-    _, err := rand.Read(rand_buf)
+	_, err := rand.Read(randBufs)
 
-    if err != nil {
-        // currently we panic when we don't have enough randomness but it might
-        // be better to return an error instead. however I feel like other
-        // programmers might not recognize what a huge issue not having
-        // randomness is so I chose the crash and burn approach
-        panic(err)
-    }
+	if err != nil {
+		// currently we panic when we don't have enough randomness but it might
+		// be better to return an error instead. however I feel like other
+		// programmers might not recognize what a huge issue not having
+		// randomness is so I chose the crash and burn approach
+		panic(err)
+	}
 
-    fmt.Println(account.lastError())
+	fmt.Println(account.lastError())
 
-    C.olm_create_account(account.ptr, unsafe.Pointer(&rand_buf[0]), rand_len)
+	C.olm_create_account(account.ptr, unsafe.Pointer(&randBufs[0]), randLen)
 
-    return account
+	return account
 }
 
-func (a Account) lastError() (string){
-    return C.GoString(C.olm_account_last_error(a.ptr))
+func (a Account) lastError() string {
+	return C.GoString(C.olm_account_last_error(a.ptr))
 }
 
-func (a Account) Pickle(key string) (string){
-    key_buf := []byte(key)
-    pickle_buffer := make([]byte, C.olm_pickle_account_length(a.ptr))
+func (a Account) Pickle(key string) string {
+	keyBufs := []byte(key)
+	pickleBuffers := make([]byte, C.olm_pickle_account_length(a.ptr))
 
-    // this returns a result we should probably inspect
-    C.olm_pickle_account(
-        a.ptr,
-        unsafe.Pointer(&key_buf[0]), C.size_t(len(key_buf)),
-        unsafe.Pointer(&pickle_buffer[0]), C.size_t(len(pickle_buffer)),
-    )
+	// this returns a result we should probably inspect
+	C.olm_pickle_account(
+		a.ptr,
+		unsafe.Pointer(&keyBufs[0]), C.size_t(len(keyBufs)),
+		unsafe.Pointer(&pickleBuffers[0]), C.size_t(len(pickleBuffers)),
+	)
 
-    return string(pickle_buffer)
+	return string(pickleBuffers)
 }
 
-func (a Account) GetIdentityKeys() (string){
-    out_length := C.olm_account_identity_keys_length(a.ptr)
-    out_buffer := make([]byte, out_length)
-    C.olm_account_identity_keys(
-        a.ptr,
-        unsafe.Pointer(&out_buffer[0]), out_length,
-    )
-    // JSON, could parse
-    return string(out_buffer)
+func (a Account) GetIdentityKeys() (IdentityKeys, error) {
+	outLength := C.olm_account_identity_keys_length(a.ptr)
+	outBuffers := make([]byte, outLength)
+	C.olm_account_identity_keys(
+		a.ptr,
+		unsafe.Pointer(&outBuffers[0]), outLength,
+	)
+	log.Println(string(outBuffers))
+
+	var keys IdentityKeys
+	if err := json.Unmarshal(outBuffers, &keys); err != nil {
+		return keys, err
+	}
+	return keys, nil
 }
 
-func (a Account) Sign(message string) (string){
-    message_buf := []byte(message)
-    out_length := C.olm_account_signature_length(a.ptr)
-    out_buffer := make([]byte, out_length)
-    C.olm_account_sign(
-        a.ptr,
-        unsafe.Pointer(&message_buf[0]), C.size_t(len(message_buf)),
-        unsafe.Pointer(&out_buffer[0]), out_length,
-    )
-    return string(out_buffer)
+func (a Account) Sign(message string) string {
+	messageBufs := []byte(message)
+	outLength := C.olm_account_signature_length(a.ptr)
+	outBuffers := make([]byte, outLength)
+	C.olm_account_sign(
+		a.ptr,
+		unsafe.Pointer(&messageBufs[0]), C.size_t(len(messageBufs)),
+		unsafe.Pointer(&outBuffers[0]), outLength,
+	)
+	return string(outBuffers)
 }
 
-func (a Account) GetOneTimeKeys() (string){
-    out_length := C.olm_account_one_time_keys_length(a.ptr)
-    out_buffer := make([]byte, out_length)
-    C.olm_account_one_time_keys(
-        a.ptr,
-        unsafe.Pointer(&out_buffer[0]), out_length,
-    )
-    // JSON, could parse
-    return string(out_buffer)
+func (a Account) GetOneTimeKeys() (OneTimeKeys, error) {
+	outLength := C.olm_account_one_time_keys_length(a.ptr)
+	outBuffers := make([]byte, outLength)
+	C.olm_account_one_time_keys(
+		a.ptr,
+		unsafe.Pointer(&outBuffers[0]), outLength,
+	)
+	var keys OneTimeKeys
+	if err := json.Unmarshal(outBuffers, &keys); err != nil {
+		return keys, err
+	}
+	return keys, nil
 }
 
 func (a Account) MarkKeysAsPublished() {
-    C.olm_account_mark_keys_as_published(a.ptr)
+	C.olm_account_mark_keys_as_published(a.ptr)
 }
 
-func (a Account) GetMaxNumberOfOneTimeKeys() (int){
-    return int(C.olm_account_mark_keys_as_published(a.ptr))
+func (a Account) GetMaxNumberOfOneTimeKeys() int {
+	return int(C.olm_account_mark_keys_as_published(a.ptr))
 }
 
-func (a Account) GenerateOneTimeKeys(count int){
-    rand_len := C.olm_account_generate_one_time_keys_random_length(
-        a.ptr, C.size_t(count),
-    )
-    rand_buf := make([]byte, rand_len)
+func (a Account) GenerateOneTimeKeys(count int) {
+	randLen := C.olm_account_generate_one_time_keys_random_length(
+		a.ptr, C.size_t(count),
+	)
+	randBufs := make([]byte, randLen)
 
-    _, err := rand.Read(rand_buf)
+	_, err := rand.Read(randBufs)
 
-    if err != nil {
-        // currently we panic when we don't have enough randomness but it might
-        // be better to return an error instead. however I feel like other
-        // programmers might not recognize what a huge issue not having
-        // randomness is so I chose the crash and burn approach
-        panic(err)
-    }
+	if err != nil {
+		// currently we panic when we don't have enough randomness but it might
+		// be better to return an error instead. however I feel like other
+		// programmers might not recognize what a huge issue not having
+		// randomness is so I chose the crash and burn approach
+		panic(err)
+	}
 
+	C.olm_account_generate_one_time_keys(
+		a.ptr, C.size_t(count),
+		unsafe.Pointer(&randBufs[0]), randLen,
+	)
 
-    C.olm_account_generate_one_time_keys(
-        a.ptr, C.size_t(count),
-        unsafe.Pointer(&rand_buf[0]), rand_len,
-    )
-
-    fmt.Println(a.lastError())
+	fmt.Println(a.lastError())
 }
